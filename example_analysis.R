b81750161d66c5f54078b9851f31bf77ae136c7f
Alex Bokov <bokov@uthscsa.edu>
2020-01-17 20:22:13 -0600

    example_analysis.R; enh; Wrote explanatory text, created plots and tables, wrote header, now writing reproducibility info

--- example_analysis.R
+++ example_analysis.R
@@ -1,5 +1,11 @@
 #' ---
-#' title: "Example Analysis"
+#' title: "Evaluating Clinithink Results"
+#' author:
+#' - "Pankil Shah"
+#' - "Alex Bokov"
+#' - "Dimpy Shah"
+#' - "Ronald Rodriguez"
+#' - "Meredith Zozus"
 #' css: "production.css"
 #' output:
 #'   html_document:
@@ -7,119 +13,161 @@
 #'     toc_float: true
 #' ---
 #'
-#' ### Settings
-#'
-#' In the below two lines are the minimum script-level settings you need.
-#' The `.projpackages` object has the names of the packages you need installed
-#' if necessary and then loaded for this scriport. The `.deps` object contains
-#' other scriports on which this one _directly_ depends (you don't need to worry
-#' about the indirect ones-- each scriport manages its own dependencies for
-#' packages and scriports). The recommended value to start with is the one shown
-#' here. You can add more if the need arises later. For more information, please
-#' see the [overview](overview.html) scriport.
-.projpackages <- c('GGally','tableone','pander','dplyr','ggplot2');
+#+ set_config, echo=FALSE, message=FALSE, warning=FALSE,results='hide'
+.projpackages <- c('GGally','pander','dplyr','ggplot2','psych','git2r');
 .deps <- c( 'dictionary.R' );
 #+ load_deps, echo=FALSE, message=FALSE, warning=FALSE,results='hide'
 # do not edit the next two lines
 .junk<-capture.output(source('./scripts/global.R',chdir=TRUE,echo=FALSE));
 .currentscript <- current_scriptname('example_analysis.R');
-if(!exists('dat01')) dat01 <- get(names(inputdata)[1]);
+#'
+#' # Data
+#'
+#' This dataset is a spreadsheet with `r ncol(dat01)` columns and
+#' `r nrow(dat01)` observations. Most of the columns represent various concepts
+#' extracted from the notes by the Clinithink system. They are numeric and range
+#' from 0 to 1, representing the weight each variable contributes to the overall
+#' score (`patientscore`).
+#'
+#' # Variables
+#'
+#' The following variables are not used in analysis and are filtered out:
+#+ c_omit, echo=FALSE
+attr(dat01,'tblinfo')$c_omit <- attr(dat01,'tblinfo')$column %in%
+  c('mrn','surname','forename','patientreporturl'
+    # the above are non-analyzable text fields
+    ,'gender' # in this dataset it's always 'U'
+    ,'other_chron_liver_disease_prm_exc' # has all 0 values except for one
+    );
+gsub('\\b_|_\\b','`',pander(v(c_omit,dat01)));
+#'
+#' The reason `other_chron_liver_disease_prm_exc` is among them is that in this
+#' dataset it always has a value of 0 except for one case.
+#'
+#' To match the instructions given to the screeners we need to consider just
+#' the NLP variables that indicate NASH, rather than the ones which reflect
+#' other eligibility criteria. Therefore we created another score, named
+#' `a_ct_nash` that is the sum of the following variables:
+#+ c_nashonly, echo=FALSE
+attr(dat01,'tblinfo')$c_nashonly <- attr(dat01,'tblinfo')$column %in%
+  c('nash_prm_inc','signs_of_nash_prm_inc','hepatic_fibrosis_nash_risk_prm_inc');
+gsub('\\b_|_\\b','`',pander(v(c_nashonly,dat01)));
+dat01$a_ct_nash <- dat01[,v(c_nashonly,dat01)] %>% rowSums;
+#'
+#' ***
+#'
+#' The following variables were used by CliniThink as exclusion criteria:
+#+ c_exclude, echo=FALSE
+attr(dat01,'tblinfo')$c_exclude <- attr(dat01,'tblinfo')$column %in%
+  grep('_exc$',map0$varname,val=TRUE);
+gsub('\\b_|_\\b','`',pander(v(c_exclude,dat01)));
+#'
+#' ***
+#'
+#' The following variables were used by CliniThink as inclusion criteria:
+#+ c_include, echo=FALSE
+attr(dat01,'tblinfo')$c_include <- attr(dat01,'tblinfo')$column %in%
+  grep('_inc$',map0$varname,val=TRUE);
+gsub('\\b_|_\\b','`',pander(v(c_include,dat01)));
+#'
+#' ***
+#'
+#' # Correlation of each pair of variables
+#'
+#' Below is a correlation matrix for each of the variables-- all the
+#' inclusion/exclusion ones (except for `other_chron_liver_disease_prm_exc` as
+#' explained above) plus `age`, `a_ct_nash` (the raw present/absent status for
+#' NASH), and `patientscore` (the score calculated via Clinithink).
+#'
+#' Red indicates strong positive correlations and dark teak indicates strong
+#' inverse correlations. The rows and columns have been sorted so as to make it
+#' easier to see clusters of correlated variables.
+#'
+#' As expected `r gsub('\\b_|_\\b','\x60',pander(v(c_nashonly,dat01)))`
+#' correlate with `a_ct_nash` (presence/absence of NASH) since the latter is
+#' their sum. Interestingly, all
+#' the large correlations for `patientscore` are negative ones, mostly for
+#' exclusion criteria.
+#'
+#+ cor01, echo=FALSE
+# Calculate a correlation matrix
+cor01 <- dat01 %>% select(-c(v(c_omit,.))) %>% cor %>%
+  {.ord<-hclust(as.dist((1-abs(.))/2))$order;(.)[.ord,.ord]};
+# Move the two score variables to the last two rows and columns for easier
+# interpretation
+cor01 <- c('a_ct_nash','patientscore') %>% c(Filter(function(xx) !xx %in% .
+                                                    ,colnames(cor01)),.) %>%
+  `[`(cor01,.,.);
+select(dat01,-c(v(c_omit,dat01))) %>% ggcorr(cor_matrix=cor01,label=TRUE
+                                             ,label_size=2,nudge_x=-8
+                                             ,layout.exp=8,nbreaks = 5, size=3);
+#'
+#' ***
+#'
+#' # NASH vs Clinithink Score
+#'
+#' In the above figure we also see that there is no correlation between
+#' `a_ct_nash` and `patientscore`. In the table below we can see in more
+#' detail considerable discrepancy between them (the italicized values).
+#+ nash_v_score, echo=FALSE
+with(dat01,table(`NASH Present:`=factor(a_ct_nash,levels=c(0,.5,1.5)
+                                        ,labels=c('No','Maybe','Yes'))
+                 ,`Clinithink Score:`=cut(patientscore,c(-Inf,0,Inf)
+                                          ,labels = c('<=0','>0')))) %>%
+  ftable %>% pander(justify='right',emphasize.strong.rows = 1:2
+                    ,emphasize.cells=cbind(c(3,5),c(4,3)));
+#' ***
+#'
+#' Below are estimates for $\kappa$ and weighted $\kappa$ again showing very
+#' little agreement between detection of NASH via Clinithink's NLP (`a_ct_nash`)
+#' versus recruitment eligibility via Clinithink's NLP (`patientscore`). The
+#' `patientscore`, a continuous variable, was binned into three quantiles for
+#' comparison with `a_ct_nash` which only has three distinct values to begin
+#' with.
+#'
+#+ kappa, echo=FALSE,warning=FALSE,message=FALSE
+mutate(dat01,a_ct_nash=(factor(a_ct_nash,levels=c(0,.5,1.5)
+                               ,labels=c('No','Maybe','Yes')))
+       ,patientscore=(cut(patientscore,quantile(patientscore,(0:3)/3)
+                          ,include.lowest = TRUE
+                          ,labels=c('No','Maybe','Yes')))) %>%
+  select(c('a_ct_nash','patientscore')) %>%
+  psych::cohen.kappa(levels = c('No','Maybe','Yes')) %>%
+  `[`(c('n.obs','agree','weight','confid')) %>%
+  setNames(c('Number of Observations','Agreement','Weights'
+             ,'$\\kappa$  Estimates'))%>% pander;
 #'
 #'
-#' ### Choosing predictor and response variables
+#' # Next steps
 #'
-#' The values below are variables arbitrarily selected for demonstration
-#' purposes. To do your actual analysis, replace `binary_outcome` and
-#' `numeric_outcome` below with vectors containing one or more column names you
-#' want to serve as the outcome variables. If you have no binary outcomes, set
-#' the value to an empty vector, `c()`. Likewise if you have no numeric
-#' outcomes.
-binary_outcome <- choose_outcomes(dat01,'c_safetf');
-binary_outcome;
-numeric_outcome <- choose_outcomes(dat01,'c_safenumeric');
-numeric_outcome;
+#' This is just a characterization of the Clinithink results, comparing two
+#' scores that are both based on different summations of Clinithink-abstracted
+#' concepts.
 #'
-#' Insuring that the binary outcomes get treated as discrete values
+#' The actual tests of greatest interest will be for the degree of agreement
+#' between a) Clinithink, b) manual review of patient charts by trained
+#' personnel, and c) a query against the structured data in the CIRD research
+#' data warehouse.
 #'
-for(ii in binary_outcome) dat01[[ii]] <- factor(dat01[[ii]]);
-#' Again, replace `predictorvars` below with vectors containing one or more
-#' column names you actually want to serve as the predictor variables
-predictorvars <- choose_predictors(dat01,'c_safe'
-                                   ,exclude=c(binary_outcome,numeric_outcome));
-predictorvars;
-#' If you are satisfied with your choice of `binary_outcome`, `numeric_outcome`,
-#' and `predictorvars` you don't need to change the following line. It simply
-#' combines the above.
-mainvars <- c(predictorvars,binary_outcome,numeric_outcome);
-mainvars;
-#' ### Plot the data
+#' We now need items b) and c). Once we have them, we can proceed with the main
+#' tests of interest.
 #'
-#' #### Explore pairwise relationships
+#' It will also be important to discover any patterns in the data that could
+#' help us understand or at least predict the greatest discrepancies between the
+#' three screening methods.
 #'
-#' A plot of all pairwise relationships between the variables of interest.
-#+ ggpairs_plot, message=FALSE, warning=FALSE, cache=TRUE
-
-# select just the columns in 'mainvars' (otherwise the plot will take forever)
-# and turn the ones tagged as 'ordinal' into factors
-dat01[,mainvars] %>% mutate_at(.,v(c_ordinal,.),factor) %>%
-  # now make a scatterplot matrix, using the first 'binary_outcome' to assign
-  # color
-  ggpairs(.,aes_string(color=binary_outcome[1]));
+#' # Reproducibility of these Results
 #'
-#' Note: if your data does not have a binary outcome, an artificial value is
-#' randomly generated for demonstration purposes.
+#+ git, echo=FALSE, message=FALSE, warning=FALSE, results='hide'
+.repourl <- 'https://github.com/bokov/nlp_nash/';
+.repo <- try(repository_head(repository(proj_get())));
+if(!is(.repo,'try-error') && !is_detached(.repo$repo)){
+  .repourl <- paste0(.repourl,'tree/',.repo$name);
+  .sha <- substr(branch_target(.repo),1,7);
+}
+#' This report is directly generated from R scripts stored in the
+#' [bokov/nlp_nash](`r paste0(.repourl,'tree/',.repo$name)`) repository on GitHub. This
 #'
-#' #### Exploring group differences.
-#'
-#' The `r sprintf("\x60%s\x60",binary_outcome[1])`
-#' variable is again used to stratify the groups to create a cohort table that
-#' can, among other things, help identify potential confounders and unexpected
-#' explanatory variables.
-#'
-pander(print(CreateTableOne(vars = mainvars, strata = binary_outcome[1]
-                            ,data=dat01, includeNA=TRUE, test=FALSE)
-             , printToggle=FALSE)
-       , caption='Cohort Characterization');
-#'
-#' ### Specify the statistical models
-#'
-#' #### Numeric outcome
-#'
-#' Formulas for the numeric outcome models.
-#+ numfrm, results='asis'
-num_formulas <- c();
-for(yy in numeric_outcome) for(xx in predictorvars){
-  num_formulas <- c(num_formulas,sprintf('%s ~ %s',yy,xx))};
-pander(cbind(num_formulas),justify='l');
-
-#' Linear regression models fitted to the above formulas.
-num_models <- sapply(num_formulas,function(xx) lm(xx,dat01) %>% update(.~.)
-                     ,simplify=FALSE);
-#' The results of the univariate model fits
-#+ pandernum,results='asis'
-for(xx in num_models) {cat('***','\n'); cat(pander(xx)); cat('***','\n');};
-#'
-#' #### Binary outcome
-#'
-#' Formulas for the binary outcome models
-#+ binfrm, results='asis'
-bin_formulas <- c();
-for(yy in binary_outcome) for(xx in predictorvars){
-  bin_formulas <- c(bin_formulas,sprintf('%s ~ %s',yy,xx))};
-pander(cbind(bin_formulas),justify='l');
-#' Logistic regression models fitted to the above formulas.
-bin_models <- sapply(bin_formulas,function(xx){
-  glm(xx,dat01,family='binomial') %>% update(.~.)},simplify=FALSE);
-#' The results of the univariate model fits
-#+ panderbin,results='asis'
-for(xx in bin_models) {cat('***','\n'); cat(pander(xx)); cat('***','\n');};
-#'
-#'
-#' ### Save results
-#'
-#' Now the results are saved and available for use by other scriports if you
-#' place `r sprintf("\x60'%s'\x60",.currentscript)` among the values in their
-#' `.deps` variables.
+#+ save,echo=FALSE,results='hide'
 save(file=paste0(.currentscript,'.rdata'),list=setdiff(ls(),.origfiles));
-#+ echo=FALSE, results='hide'
 c()
